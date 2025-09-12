5b6bfbb4496179a42f38d97f648b04084a4e7a7d
Avn3s <astarcys7@proton.me>
2025-09-13 00:48:57 +0530

    figured out basic hotkey and abbreviation creation

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-from keyboard import add_hotkey
+import keyboard
+from time import sleep
+from pyperclip import paste
+
+
+def comment():
+    keyboard.send("home, #, space,end")
+
+
+closes = {"(": ")", "{": "}", "[": "]", '"': '"', "'": "'", "<": ">"}
+
+
+def close(open):
+    keyboard.write(closes[open])
+    sleep(0.2)
+    keyboard.send("left")
+
+
+# add_word_listener("(", insert_closed_bracket,triggers=['typing'])
+keyboard.add_hotkey("shift+9", close, args=["("])
+
+keyboard.add_abbreviation("*mail", "astarcys7@proton.me")
+keyboard.wait()
